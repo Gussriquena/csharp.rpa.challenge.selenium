546deebe75cc20e560815fa42f307a4347cd7ee4
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-04-05 19:08:18 -0300

    Adicionar arquivos de projeto.

---
+++ csharp.rpa.challenge.selenium/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace csharp.rpa.challenge.selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+            driver = new ChromeDriver(@"C:\Projetos\RPA\csharp.rpa.challenge.selenium\csharp.rpa.challenge.selenium\resources\");
+            driver.Manage().Window.Maximize();
+            String baseURL = "https://www.google.com.br";
+
+            driver.Navigate().GoToUrl(baseURL);
+        }
+    }
+}
