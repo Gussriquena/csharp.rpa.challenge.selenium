4716e197c24416905230265e1da65ccf73647f78
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-04-05 20:47:22 -0300

    selenium inicial setup with config files and log4net

--- csharp.rpa.challenge.selenium/Program.cs
+++ csharp.rpa.challenge.selenium/Program.cs
@@ -1,19 +1,27 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.IO;
 
 namespace csharp.rpa.challenge.selenium
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            log.Info("Starting application");
+            var iniFile = new IniFile(@"C:\Projetos\RPA\csharp.rpa.challenge.selenium\csharp.rpa.challenge.selenium\resources\config.ini");
+
             IWebDriver driver;
             driver = new ChromeDriver(@"C:\Projetos\RPA\csharp.rpa.challenge.selenium\csharp.rpa.challenge.selenium\resources\");
             driver.Manage().Window.Maximize();
-            String baseURL = "https://www.google.com.br";
+            String baseURL = iniFile.GetValue("url", "mainUrlChallenge");
 
             driver.Navigate().GoToUrl(baseURL);
+            log.Info("Opened website");
+
         }
     }
 }
